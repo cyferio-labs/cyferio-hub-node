3fab8653bab415c980b711ba35b305cc52dbb764
vladilen11 <vladilenaksana@163.com>
2024-10-21 04:58:24 +0800

    update sui response

--- pallets/offchain-worker/src/lib.rs
+++ pallets/offchain-worker/src/lib.rs
@@ -15,11 +15,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//! <!-- markdown-link-check-disable -->
 //! # Offchain Worker Example Pallet
 //!
-//! The Offchain Worker Example: A simple pallet demonstrating
-//! concepts, APIs and structures common to most offchain workers.
+//! This pallet demonstrates concepts, APIs and structures common to most offchain workers.
 //!
 //! Run `cargo doc --package pallet-example-offchain-worker --open` to view this module's
 //! documentation.
@@ -28,20 +26,14 @@
 //! - [`Call`]
 //! - [`Pallet`]
 //!
-//! **This pallet serves as an example showcasing Substrate off-chain worker and is not meant to
-//! be used in production.**
+//! **This pallet serves as an example and is not meant for production use.**
 //!
 //! ## Overview
 //!
-//! In this example we are going to build a very simplistic, naive and definitely NOT
-//! production-ready oracle for BTC/USD price.
-//! Offchain Worker (OCW) will be triggered after every block, fetch the current price
-//! and prepare either signed or unsigned transaction to feed the result back on chain.
-//! The on-chain logic will simply aggregate the results and store last `64` values to compute
-//! the average price.
-//! Additional logic in OCW is put in place to prevent spamming the network with both signed
-//! and unsigned transactions, and custom `UnsignedValidator` makes sure that there is only
-//! one unsigned transaction floating in the network.
+//! This pallet implements a simple oracle for BTC/USD price and a task processing system.
+//! The Offchain Worker (OCW) is triggered after every block to fetch the current price
+//! and process any pending tasks. It prepares either signed or unsigned transactions
+//! to feed the results back on-chain.
 
 #![cfg_attr(not(feature = "std"), no_std)]
 
@@ -88,6 +80,7 @@ use sp_std::fmt::Debug;
 #[cfg(test)]
 mod tests;
 
+/// Defines a task with a data availability height, blob data, and processing status.
 #[derive(Clone, Encode, Decode, Eq, PartialEq, RuntimeDebug, Default, TypeInfo, MaxEncodedLen)]
 #[scale_info(skip_type_params(StringLimit))]
 pub struct Task<StringLimit: Get<u32> + Clone> {
@@ -97,17 +90,9 @@ pub struct Task<StringLimit: Get<u32> + Clone> {
 }
 
 /// Defines application identifier for crypto keys of this module.
-///
-/// Every module that deals with signatures needs to declare its unique identifier for
-/// its crypto keys.
-/// When offchain worker is signing transactions it's going to request keys of type
-/// `KeyTypeId` from the keystore and use the ones it finds to sign the transaction.
-/// The keys can be inserted manually via RPC (see `author_insertKey`).
 pub const KEY_TYPE: KeyTypeId = KeyTypeId(*b"btc!");
 
-/// Based on the above `KeyTypeId` we need to generate a pallet-specific crypto type wrappers.
-/// We can use from supported crypto kinds (`sr25519`, `ed25519` and `ecdsa`) and augment
-/// the types with this pallet-specific identifier.
+/// Crypto wrapper for this module's keys.
 pub mod crypto {
 	use super::KEY_TYPE;
 	use sp_core::sr25519::Signature as Sr25519Signature;
@@ -120,13 +105,14 @@ pub mod crypto {
 
 	pub struct TestAuthId;
 
+	// Implementation of traits for app crypto
 	impl frame_system::offchain::AppCrypto<MultiSigner, MultiSignature> for TestAuthId {
 		type RuntimeAppPublic = Public;
 		type GenericSignature = sp_core::sr25519::Signature;
 		type GenericPublic = sp_core::sr25519::Public;
 	}
 
-	// implemented for mock runtime in test
+	// Implementation for mock runtime in test
 	impl frame_system::offchain::AppCrypto<<Sr25519Signature as Verify>::Signer, Sr25519Signature>
 		for TestAuthId
 	{
@@ -138,21 +124,31 @@ pub mod crypto {
 
 pub use pallet::*;
 
+/// Struct to deserialize the response from the Sui API
 #[derive(Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 struct ResponseData {
     is_succ: bool,
     #[serde(default)]
-    res: Option<TimeData>,
+    res: Option<SuiData>,
     #[serde(default)]
     err: Option<ErrorData>,
 }
 
+/// Struct to hold Sui-specific data
+#[derive(Deserialize, Debug)]
+struct SuiData {
+    sui_digest: String,
+    time: String,
+}
+
+/// Struct to hold time data
 #[derive(Deserialize, Debug)]
 struct TimeData {
     time: String,
 }
 
+/// Struct to hold error data
 #[derive(Deserialize, Debug)]
 struct ErrorData {
     message: String,
@@ -169,7 +165,7 @@ pub mod pallet {
 	use frame_support::pallet_prelude::*;
 	use frame_system::pallet_prelude::*;
 
-	/// This pallet's configuration trait
+	/// Configuration trait for this pallet.
 	#[pallet::config]
 	pub trait Config: CreateSignedTransaction<Call<Self>> + frame_system::Config {
 		/// The identifier type for an offchain worker.
@@ -178,27 +174,15 @@ pub mod pallet {
 		/// The overarching event type.
 		type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
 
-		// Configuration parameters
-
-		/// A grace period after we send transaction.
-		///
-		/// To avoid sending too many transactions, we only attempt to send one
-		/// every `GRACE_PERIOD` blocks. We use Local Storage to coordinate
-		/// sending between distinct runs of this offchain worker.
+		/// Grace period after sending a transaction.
 		#[pallet::constant]
 		type GracePeriod: Get<BlockNumberFor<Self>>;
 
 		/// Number of blocks of cooldown after unsigned transaction is included.
-		///
-		/// This ensures that we only accept unsigned transactions once, every `UnsignedInterval`
-		/// blocks.
 		#[pallet::constant]
 		type UnsignedInterval: Get<BlockNumberFor<Self>>;
 
 		/// A configuration for base priority of unsigned transactions.
-		///
-		/// This is exposed so that it can be tuned for particular runtime, when
-		/// multiple pallets send unsigned transactions.
 		#[pallet::constant]
 		type UnsignedPriority: Get<TransactionPriority>;
 
@@ -217,14 +201,6 @@ pub mod pallet {
 	#[pallet::hooks]
 	impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {
 		/// Offchain Worker entry point.
-		///
-		/// By implementing `fn offchain_worker` you declare a new offchain worker.
-		/// This function will be called when the node is fully synced and a new best block is
-		/// successfully imported.
-		/// Note that it's not guaranteed for offchain workers to run on EVERY block, there might
-		/// be cases where some blocks are skipped, or for some the worker runs twice (re-orgs),
-		/// so the code should be able to handle that.
-		/// You can use `Local Storage` API to coordinate runs of the worker.
 		fn offchain_worker(block_number: BlockNumberFor<T>) {
 			log::info!("Offchain worker started at block: {:?}", block_number);
 
@@ -244,9 +220,10 @@ pub mod pallet {
 		}
 	}
 
-	/// A public part of the pallet.
+	/// Dispatchable functions for this pallet.
 	#[pallet::call]
 	impl<T: Config> Pallet<T> {
+		/// Submit a new task to be processed.
 		#[pallet::call_index(0)]
 		#[pallet::weight({0})]
 		pub fn submit_task(origin: OriginFor<T>, da_height: u64, blob: Vec<u8>) -> DispatchResult {
@@ -269,53 +246,97 @@ pub mod pallet {
 					.map_err(|_| Error::<T>::TooManyTasks)
 			})?;
 
-			TaskHistory::<T>::insert(da_height, (bounded_blob, false));
+			TaskHistory::<T>::insert(da_height, (bounded_blob, false, None::<BoundedVec<u8, T::StringLimit>>, None::<BoundedVec<u8, T::StringLimit>>));
 			
-			// 更新最后提交的 da_height
+			// Update the last submitted da_height
 			LastDaHeight::<T>::put(da_height);
 
 			Self::deposit_event(Event::TaskSubmitted { da_height: new_task.da_height, blob: new_task.blob });
 			Ok(())
 		}
 
+		/// Process a task and submit the result as an unsigned transaction.
 		#[pallet::call_index(1)]
 		#[pallet::weight({0})]
 		pub fn process_task_unsigned(
 			origin: OriginFor<T>,
 			block_number: BlockNumberFor<T>,
 			task_index: u32,
+			sui_digest: Vec<u8>,
+			time: Vec<u8>,
 		) -> DispatchResultWithPostInfo {
 			ensure_none(origin)?;
-			Self::remove_task(task_index);
-			let current_block = <system::Pallet<T>>::block_number();
 
+			let bounded_sui_digest: BoundedVec<u8, T::StringLimit> = 
+				sui_digest.try_into().map_err(|_| Error::<T>::DigestTooLong)?;
+			let bounded_time: BoundedVec<u8, T::StringLimit> = 
+				time.try_into().map_err(|_| Error::<T>::TimeTooLong)?;
+
+			Tasks::<T>::mutate(|tasks| {
+				if let Some(task) = tasks.get_mut(task_index as usize) {
+					task.processed = true;
+					TaskHistory::<T>::insert(
+						task.da_height,
+						(task.blob.clone(), true, Some(bounded_sui_digest.clone()), Some(bounded_time.clone()))
+					);
+
+					Self::deposit_event(Event::TaskProcessed { 
+						da_height: task.da_height, 
+						blob: task.blob.clone(),
+						sui_digest: Some(bounded_sui_digest),
+						time: Some(bounded_time)
+					});
+				}
+			});
+
+			Self::remove_task(task_index);
+			
+			let current_block = <system::Pallet<T>>::block_number();
 			<NextUnsignedAt<T>>::put(current_block + T::UnsignedInterval::get());
+			
 			Ok(().into())
 		}
 	}
 
-	/// Events for the pallet.
+	/// Events for this pallet.
 	#[pallet::event]
 	#[pallet::generate_deposit(pub(super) fn deposit_event)]
 	pub enum Event<T: Config> {
+        /// A new task has been submitted.
         TaskSubmitted{da_height: u64, blob: BoundedVec<u8, T::StringLimit>},
-        TaskProcessed{da_height: u64, blob: BoundedVec<u8, T::StringLimit>},
+        /// A task has been processed.
+        TaskProcessed{
+            da_height: u64, 
+            blob: BoundedVec<u8, T::StringLimit>,
+            sui_digest: Option<BoundedVec<u8, T::StringLimit>>,
+            time: Option<BoundedVec<u8, T::StringLimit>>
+        },
 	}
 
+	/// Errors for this pallet.
 	#[pallet::error]
 	pub enum Error<T> {
+		/// Too many tasks have been submitted.
 		TooManyTasks,
+		/// The DA height is too long.
 		DaHeightTooLong,
+		/// The blob is too long.
 		BlobTooLong,
+		/// The DA height is invalid.
 		InvalidDaHeight,
+		/// The digest is too long.
+		DigestTooLong,
+		/// The time is too long.
+		TimeTooLong,
 	}
 
+	/// Validate unsigned transactions.
 	#[pallet::validate_unsigned]
 	impl<T: Config> ValidateUnsigned for Pallet<T> {
 		type Call = Call<T>;
 
 		fn validate_unsigned(_source: TransactionSource, call: &Self::Call) -> TransactionValidity {
-			if let Call::process_task_unsigned { block_number, task_index } = call {
+			if let Call::process_task_unsigned { block_number, task_index, sui_digest: _, time: _ } = call {
 				// Check if it's time to submit a new unsigned transaction
 				let current_block = <system::Pallet<T>>::block_number();
 				let next_unsigned_at = <NextUnsignedAt<T>>::get();
@@ -341,30 +362,33 @@ pub mod pallet {
 		}
 	}
 
+	/// Storage for tasks.
 	#[pallet::storage]
 	#[pallet::getter(fn tasks)]
 	pub type Tasks<T: Config> = StorageValue<_, BoundedVec<Task<T::StringLimit>, T::MaxTasks>, ValueQuery>;
 
+	/// Storage for task history.
 	#[pallet::storage]
 	#[pallet::getter(fn task_by_height)]
 	pub type TaskHistory<T: Config> = StorageMap<
 		_,
 		Blake2_128Concat,
 		u64,  // da_height as key
-		(BoundedVec<u8, T::StringLimit>, bool),  // (blob, processed) as value
+		(BoundedVec<u8, T::StringLimit>, bool, Option<BoundedVec<u8, T::StringLimit>>, Option<BoundedVec<u8, T::StringLimit>>),  // (blob, processed, sui_digest, time) as value
 		ValueQuery
 	>;
 
+	/// Storage for the next block to submit an unsigned transaction.
 	#[pallet::storage]
 	pub(super) type NextUnsignedAt<T: Config> = StorageValue<_, BlockNumberFor<T>, ValueQuery>;
 
+	/// Storage for the last processed DA height.
 	#[pallet::storage]
 	#[pallet::getter(fn last_da_height)]
 	pub type LastDaHeight<T: Config> = StorageValue<_, u64, ValueQuery>;
 }
 
-/// Payload used by this example crate to hold price
-/// data required to submit a transaction.
+/// Payload used by this example crate to hold price data required to submit a transaction.
 #[derive(Encode, Decode, Clone, PartialEq, Eq, RuntimeDebug, scale_info::TypeInfo)]
 pub struct PricePayload<Public, BlockNumber> {
 	block_number: BlockNumber,
@@ -379,6 +403,7 @@ impl<T: SigningTypes> SignedPayload<T> for PricePayload<T::Public, BlockNumberFo
 }
 
 impl<T: Config> Pallet<T> {
+    /// Process a task by fetching Sui data and submitting an unsigned transaction.
     fn process_task(
 		block_number: BlockNumberFor<T>,
 		task_index: u32, 
@@ -386,32 +411,31 @@ impl<T: Config> Pallet<T> {
 	) -> Result<(), &'static str> {
         log::info!("Processing task: {:?}", task.blob.clone());
         
-        match Self::test_sui(task.da_height, task.blob.clone()) {
-            Ok(gas_price) => {
-                log::info!("Sui gas price: {:?}", gas_price);
+        match Self::fetch_sui_data(task.da_height, task.blob.clone()) {
+            Ok((sui_digest, time)) => {
+                log::info!("Sui digest: {:?}, Time: {:?}", sui_digest, time);
                 
-                // Update task status
-                Tasks::<T>::mutate(|tasks| {
-                    if let Some(task) = tasks.get_mut(task_index as usize) {
-                        task.processed = true;
-                    }
-                });
-
-                Self::deposit_event(Event::TaskProcessed { da_height: task.da_height, blob: task.blob.clone() });
-                
-                let call = Call::process_task_unsigned { block_number, task_index };
+                // Submit unsigned transaction with sui_digest and time
+                let call = Call::process_task_unsigned { 
+                    block_number, 
+                    task_index,
+                    sui_digest: sui_digest.into_bytes(),
+                    time: time.into_bytes(),
+                };
 
                 SubmitTransaction::<T, Call<T>>::submit_unsigned_transaction(call.into())
                     .map_err(|()| "Unable to submit unsigned transaction")?;
+
                 Ok(())
             },
             Err(e) => {
-                log::error!("Error getting Sui gas price: {:?}", e);
-                Err("Failed to get Sui gas price")
+                log::error!("Error getting Sui data: {:?}", e);
+                Err("Failed to get Sui data")
             }
         }
     }
 
+    /// Remove a processed task from storage and update its history.
     fn remove_task(task_index: u32) {
         let mut da_height = None;
         Tasks::<T>::mutate(|tasks| {
@@ -424,45 +448,15 @@ impl<T: Config> Pallet<T> {
         });
 
         if let Some(height) = da_height {
-            // Update processing status
+            // Update processing status in task history
             TaskHistory::<T>::mutate(height, |task| {
-                let (blob, _) = task;
-                *task = (blob.clone(), true);
+                let (blob, _, sui_digest, time) = task;
+                *task = (blob.clone(), true, sui_digest.clone(), time.clone());
             });
         }
     }
-
-	fn fetch_price() -> Result<u32, http::Error> {
-		let deadline = sp_io::offchain::timestamp().add(Duration::from_millis(2_000));
-		let request =
-			http::Request::get("https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD");
-		let pending = request.deadline(deadline).send().map_err(|_| http::Error::IoError)?;
-		let response = pending.try_wait(deadline).map_err(|_| http::Error::DeadlineReached)??;
-		if response.code != 200 {
-			log::warn!("Unexpected status code: {}", response.code);
-			return Err(http::Error::Unknown)
-		}
-
-		let body = response.body().collect::<Vec<u8>>();
-		let body_str = alloc::str::from_utf8(&body).map_err(|_| {
-			log::warn!("No UTF8 body");
-			http::Error::Unknown
-		})?;
-
-		let price = match Self::parse_price(body_str) {
-			Some(price) => Ok(price),
-			None => {
-				log::warn!("Unable to extract price from the response: {:?}", body_str);
-				Err(http::Error::Unknown)
-			},
-		}?;
-
-		log::warn!("Got price: {} cents", price);
-
-		Ok(price)
-	}
-
-	fn test_sui(da_height: u64, blob: BoundedVec<u8, T::StringLimit>) -> Result<u32, http::Error> {
+	/// Test the Sui API by sending a request with task data.
+	fn fetch_sui_data(da_height: u64, blob: BoundedVec<u8, T::StringLimit>) -> Result<(String, String), http::Error> {
 		let deadline = sp_io::offchain::timestamp().add(Duration::from_millis(2_000));
 		let url = "http://47.236.78.251:3000/v1/Warlus/Store";
 		
@@ -472,7 +466,7 @@ impl<T: Config> Pallet<T> {
 		})?;
 		
 		let request_body = format!(
-			r#"{{"da_height": {}, "blob": "{}", "epochs": 1}}"#,
+			r#"{{"da_height": "{}", "blob": "{}", "epochs": 1}}"#,
 			da_height, blob_str
 		);
 		
@@ -520,33 +514,13 @@ impl<T: Config> Pallet<T> {
 			return Err(http::Error::Unknown);
 		}
 
-		let time = response_json.res.ok_or_else(|| {
+		let sui_data = response_json.res.ok_or_else(|| {
 			log::error!("Missing 'res' field in successful response");
 			http::Error::Unknown
-		})?.time;
+		})?;
 
-		log::info!("Time retrieved from Sui: {}", time);
-	
-		Ok(1u32) // Return a fixed value, you can modify this based on your actual requirements
-	}
-		
-	/// Parse the price from the given JSON string using `lite-json`.
-	/// Returns `None` when parsing failed or `Some(price in cents)` when parsing is successful.
-	fn parse_price(price_str: &str) -> Option<u32> {
-		let val = lite_json::parse_json(price_str);
-		let price = match val.ok()? {
-			JsonValue::Object(obj) => {
-				let (_, v) = obj.into_iter().find(|(k, _)| k.iter().copied().eq("USD".chars()))?;
-				match v {
-					JsonValue::Number(number) => number,
-					_ => return None,
-				}
-			},
-			_ => return None,
-		};
+		log::info!("Sui digest: {}, Time: {}", sui_data.sui_digest, sui_data.time);
 
-		let exp = price.fraction_length.saturating_sub(2);
-		Some(price.integer as u32 * 100 + (price.fraction / 10_u64.pow(exp)) as u32)
+		Ok((sui_data.sui_digest, sui_data.time))
 	}
 }
-
